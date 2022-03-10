d9817d65ebfeceb71de3f948eeba637fcd2405b4
dac392 <dac392@scarletmail.rutgers.edu>
2022-03-09 22:47:45 -0500

    schedule tester

--- Schedule_tester.py
+++ Schedule_tester.py
@@ -1,15 +1,24 @@
-import Schedule.py
+from Schedule import *
 import concurrent.futures
 
 from os import listdir
 from os.path import isfile, join
-
+import os
 
 
 
 BENCH_PATH = "../benchmarks/"
 modes = ['-a','-b','-c']
 
-benchmarks = [f for f in listdir(BENCH_PATH) if isfile(join(mypath, f))]
-with concurrent.futures.ProcessPoolExcecutor() as executor:
-	executor.map(testing_function, benchmarks)
+benchmarks_a = [f for f in listdir(BENCH_PATH) if isfile(join(BENCH_PATH, f))]
+#print(f"benchmark files: {benchmarks}")
+#print(benchmarks)
+benchmarks = [f for f in benchmarks_a if "bench" in f]
+print(f"benchmarks: {benchmarks}")
+with concurrent.futures.ProcessPoolExecutor() as executor:
+        executor.map(testing_function, benchmarks)
+
+testing_files = [f for f in listdir("./results/") if isfile(join("./results",f))]
+for file in testing_files:
+        os.system(f"../ILOC_Simulator/sim < ./results/{file} > ./iloc_results/{file}")
+print("Done")
